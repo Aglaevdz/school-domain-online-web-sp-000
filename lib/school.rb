eee2539bc9b4cbf6bbbc8a322d18710b2a5db67d
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-06 00:48:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,10 +7,8 @@ class School
   end
 
   def add_student(name, grade)
-    if roster.has_key?(grade)
-      roster[grade] << name
-    else
-      roster[grade] = [name]
+self.roster[grade] ||= []
+self.roster[grade] << name
     end
   end
 
